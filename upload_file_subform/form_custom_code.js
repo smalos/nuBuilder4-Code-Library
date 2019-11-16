a93d1326415fba9559a4348708d6b5b0fe2e34e1
smalos <45562597+smalos@users.noreply.github.com>
2019-11-16 21:53:17 +0100

    Update form_custom_code.js

--- upload_file_subform/form_custom_code.js
+++ upload_file_subform/form_custom_code.js
@@ -5,8 +5,8 @@ function upload(event) {
     var td = $(event.target);
     var t = td.attr('data-nu-prefix');
 
-    fileField = t + 'sample_file_name';
-    fileId = t + 'sample_file_id';
+    fileField = t + 'files_file_name';
+    fileId = t + 'files_file_id';
 
     $("#fileToUpload").click();
 
