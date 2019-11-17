43b819a9ac48db5c65e4ae64d61cab26271265a5
smalos <45562597+smalos@users.noreply.github.com>
2019-11-17 09:27:11 +0100

    Update form_custom_code.js

--- upload_file_subform/form_custom_code.js
+++ upload_file_subform/form_custom_code.js
@@ -1,26 +1,35 @@
-var fileField;
-var fileId;
+// ** Change accordingly:
+// *****************************************
+
+var uploadFolder = 'libs/upload/documents/';
+var fileNameId = 'files_file_name';
+var fileIdId = 'files_file_id';
+var subFormId = 'sample_files';
+
+// *****************************************
+
+var fileNameRowId;
+var fileIdRowId;
 
 function uploadFile(event) {
-
     var td = $(event.target);
     var t = td.attr('data-nu-prefix');
 
-    fileField = t + 'files_file_name';
-    fileId = t + 'files_file_id';
+    fileNameRowId = t + fileNameId;
+    fileIdRowId = t + fileIdId;
 
     $("#fileToUpload").click();
 
 }
 
 function createDownloadLink(field, folder, fileId, fileName) {
-
+debugger;
     $('#' + field)
         .css({
             "text-decoration": "underline"
         })
         .css('cursor', 'pointer')
-        .off('click') //.prop('onclick',null).off('click');
+        .off('click')
         .attr({
             fileName: fileName,
             fileId: fileId,
@@ -33,16 +42,36 @@ function createDownloadLink(field, folder, fileId, fileName) {
 }
 
 
+function addDownloadLinks(subform) {
+   
+    var sf = nuSubformObject(subform);
+    var cId = sf.fields.indexOf(fileIdId);
+	var cName = sf.fields.indexOf(fileNameId);
+
+    for (var i = 0; i < sf.rows.length; i++) {
+
+            var fileId = sf.rows[i][cId];
+			var fileName = sf.rows[i][cName];
+
+			if (fileName !== '') {
+				createDownloadLink(subFormId + nuPad3(i) + fileNameId, uploadFolder, fileId, fileName);
+			}
+    }
+}
+
+
 function downloadFile(url, filename) {
-    
     var a = document.createElement("a");
     a.href = url;
     a.setAttribute("download", filename);
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-    
 }
 
-
-nuStopBrowserResize();
+if(nuFormType() == 'edit') {
+    addDownloadLinks(subFormId);
+} else
+{
+    nuStopBrowserResize();
+}
