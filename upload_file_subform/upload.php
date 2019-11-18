fc4990972ff7a8e8a7e2f3a8ef9c3d0eb7b1f1f0
smalos <45562597+smalos@users.noreply.github.com>
2019-11-18 20:48:39 +0100

    Update upload.php

--- upload_file_subform/upload.php
+++ upload_file_subform/upload.php
@@ -1,39 +1,58 @@
 <?php
 
+// Upload directory
 $uploaddir = './documents/';
+
+// Allowed file extensions
 $allowed = array(
     'pdf',
     'docx'
 );
 
-$filename = $_FILES['file']['name'];
-$record_id = isset($_POST['record_id']) ? $_POST['record_id'] : die("record_id missing");
-
-$ext = pathinfo($filename, PATHINFO_EXTENSION);
-$data = array();
-if (!in_array($ext, $allowed))
+try
 {
+    $data = array();
     $data['status'] = 'error';
-    $data['error_message'] = 'Invalid file type';
-    echo "invalid";
+
+    if (!isset($_FILES['file']) || !is_uploaded_file($_FILES['file']['tmp_name']) || $_FILES['file']['error'] != UPLOAD_ERR_OK)
+    {
+        $data['error'] = $_FILES['file']['error'];
+    } else
+		{	
+
+		$filename = $_FILES['file']['name'];
+		$record_id = isset($_POST['record_id']) ? $_POST['record_id'] : "";
+
+		$ext = pathinfo($filename, PATHINFO_EXTENSION);
+		if (!in_array($ext, $allowed))
+		{
+			$data['error'] = 'INVALID_FILE_TYPE';			
+		} else 
+			{	
+
+			$file_name = sanitizeFilename(basename($_FILES['file']['name']));
+			$file_id = time() . '_' . uniqid() . '_' . $record_id;
+			$file = $uploaddir . $file_id . '_' . $file_name;
+
+			if (move_uploaded_file($_FILES['file']['tmp_name'], $file))
+			{
+				$data['status'] = 'success';
+				$data['file_name'] = $file_name;
+				$data['file_id'] = $file_id;
+			}
+			else
+			{
+				$data['error'] = 'ERROR_MOVING_FILE';
+			}
+		}
+	}
+
+    echo json_encode($data);
 }
-else
+
+catch(Exception $e)
 {
-    $file_name = sanitizeFilename(basename($_FILES['file']['name']));
-    $file_id = time() . '_' . uniqid() . '_' . $record_id;
-    $file = $uploaddir . $file_id . '_' . $file_name;
-
-    if (is_uploaded_file($_FILES['file']['tmp_name']) && move_uploaded_file($_FILES['file']['tmp_name'], $file))
-    {
-        $data['status'] = 'success';
-        $data['file_name'] = $file_name;
-        $data['file_id'] = $file_id;
-    }
-    else
-    {
-        $data['status'] = 'error';
-    }
-
+    $data['error'] = $e->getMessage();
     echo json_encode($data);
 }
 
